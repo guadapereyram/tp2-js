88f1768d37386472762df8055b7b28724f12c27d
Guadalupe <guadapereyram@gmail.com>
2026-03-16 18:27:37 -0300

    Resuelve ejercicio 12: genera número aleatorio entre 1 y 99

---
+++ js/12.js
@@ -0,0 +1,5 @@
+/*12- Realiza un script que genere un número aleatorio entre 1 y 99*/
+
+const randomNumber = Math.floor(Math.random() * 99) + 1;
+
+alert(`El número aleatorio generado es ${randomNumber}.`);
